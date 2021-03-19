abf84aa2e0d2afd650313457449476b4dbb0aa57
Marichka <marichka.v.richtsi@gmail.com>
2021-03-19 23:01:07 +0200

    Create BMP_File.cs

---
+++ Project/Project/BMP_File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project
+{
+    public class BMP_File
+    {
+        private UInt32 filesize;
+        private UInt32 width;
+        private UInt32 depth;
+        
+        BMP_File(string path){}
+
+        BMP_File(string path, double numberOfTimes){}
+    }
+}
