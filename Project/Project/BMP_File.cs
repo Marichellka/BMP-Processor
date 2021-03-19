16a3ed27b7803442f9e44b98d970bfe6e9311246
Marichka <marichka.v.richtsi@gmail.com>
2021-03-19 23:42:29 +0200

    Add bmp file reader

--- Project/Project/BMP_File.cs
+++ Project/Project/BMP_File.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Project
 {
@@ -7,8 +8,29 @@ namespace Project
         private UInt32 filesize;
         private UInt32 width;
         private UInt32 depth;
-        
-        BMP_File(string path){}
+
+        BMP_File(string path)
+        {
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                Reader(streamReader, 2);//BM
+                filesize = UInt32.Parse(Reader(streamReader, 4));
+                Reader(streamReader, 12);//reserved[2], headersize, infoSize
+                width = UInt32.Parse(Reader(streamReader, 4));
+                depth = UInt32.Parse(Reader(streamReader, 4));
+                Reader(streamReader, 28);//other info
+            }
+        }
+
+        private string Reader(StreamReader sr, int numberOfBites)
+        {
+            string info = "";
+            while (numberOfBites!=0)
+            {
+                info += sr.Read();
+            }
+            return info;
+        }
 
         BMP_File(string path, double numberOfTimes){}
     }
