3b4fee337833afc4adada85972b967dbf67ca9e0
Marichka <marichka.v.richtsi@gmail.com>
2021-03-22 21:21:53 +0200

    Add Writer

--- Project/Project/BMP_File.cs
+++ Project/Project/BMP_File.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 
 namespace Project
 {
@@ -7,18 +9,20 @@ namespace Project
     {
         private UInt32 filesize;
         private Picture _picture;
+        private List<Byte> headerInfo;
 
         BMP_File(string path)
         {
+            headerInfo = new List<byte>();
             using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
             {
                 reader.ReadBytes(2);//BM
                 filesize = reader.ReadUInt32();
-                reader.ReadBytes(12);//reserved[2], headersize, infoSize
+                headerInfo.AddRange(reader.ReadBytes(12));//reserved[2], headersize, infoSize
                 UInt32 width = reader.ReadUInt32();
                 UInt32 depth = reader.ReadUInt32();
                 _picture = new Picture(width, depth);
-                reader.ReadBytes(28);//other info
+                headerInfo.AddRange(reader.ReadBytes(28));//other info
                 CreatePicture(reader);
             }
             
@@ -38,7 +42,32 @@ namespace Project
 
         BMP_File(string path, double numberOfTimes, BMP_File previousFile)
         {
-            
+            headerInfo = previousFile.headerInfo;
+            _picture = new Picture(Convert.ToUInt32(previousFile._picture.Width * numberOfTimes),
+                Convert.ToUInt32(previousFile._picture.Depth * numberOfTimes));
+            _picture.Pixels = CreateNewPicture(previousFile._picture.Pixels);
+            filesize = Convert.ToUInt32(previousFile.filesize * numberOfTimes);
+        }
+
+        private void Write(string path)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            {
+                writer.Write('B');
+                writer.Write('M');
+                writer.Write(filesize);
+                writer.Write(new Byte[]{0, 0});
+                for (int i = 2; i < 12; i++)
+                {
+                    writer.Write(headerInfo[i]);
+                }
+                writer.Write(_picture.Width);
+                writer.Write(_picture.Depth);
+                for (int i = 12; i < headerInfo.Count; i++)
+                {
+                    writer.Write(headerInfo[i]);
+                }
+            }
         }
     }
 }
