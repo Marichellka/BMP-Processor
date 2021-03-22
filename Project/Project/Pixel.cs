6fbcaf1357d902ef9c7a5e64924a19abe434a4f0
Marichka <marichka.v.richtsi@gmail.com>
2021-03-22 19:14:28 +0200

    Create Pixel.cs

---
+++ Project/Project/Pixel.cs
@@ -0,0 +1,9 @@
+﻿namespace Project
+{
+    public class Pixel
+    {
+        public int Red { get; private set;}
+        public int Green { get; private set;}
+        public int Blue { get; private set;}
+    }
+}
