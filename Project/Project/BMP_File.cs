c0366805310648baf1a7f5b4feec6596a27254d1
Marichka <marichka.v.richtsi@gmail.com>
2021-03-24 19:14:46 +0200

    Change writer

--- Project/Project/BMP_File.cs
+++ Project/Project/BMP_File.cs
@@ -11,7 +11,7 @@ namespace Project
         private Picture _picture;
         private List<Byte> headerInfo;
 
-        BMP_File(string path)
+        public BMP_File(string path)
         {
             headerInfo = new List<byte>();
             using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
@@ -40,32 +40,38 @@ namespace Project
             }
         }
 
-        BMP_File(string path, double numberOfTimes, BMP_File previousFile)
+        public BMP_File(double numberOfTimes, BMP_File previousFile)
         {
             headerInfo = previousFile.headerInfo;
             _picture = new Picture(Convert.ToUInt32(previousFile._picture.Width * numberOfTimes),
                 Convert.ToUInt32(previousFile._picture.Depth * numberOfTimes));
-            _picture.Pixels = CreateNewPicture(previousFile._picture.Pixels);
+            _picture.Pixels = _picture.CreateNewPicture(previousFile._picture.Pixels);
             filesize = Convert.ToUInt32(previousFile.filesize * numberOfTimes);
         }
 
-        private void Write(string path)
+        public void Writer(string path)
         {
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
             {
                 writer.Write('B');
                 writer.Write('M');
                 writer.Write(filesize);
-                writer.Write(new Byte[]{0, 0});
-                for (int i = 2; i < 12; i++)
+                for (int i = 0; i < 12; i++) //reserved[2], headersize, infoSize
                 {
                     writer.Write(headerInfo[i]);
                 }
                 writer.Write(_picture.Width);
                 writer.Write(_picture.Depth);
-                for (int i = 12; i < headerInfo.Count; i++)
+                for (int i = 12; i < headerInfo.Count; i++) //other info
                 {
                     writer.Write(headerInfo[i]);
+                }
+
+                foreach (var currentPixel in _picture.Pixels)
+                {
+                    writer.Write(currentPixel.Red);
+                    writer.Write(currentPixel.Green);
+                    writer.Write(currentPixel.Blue);
                 }
             }
         }
