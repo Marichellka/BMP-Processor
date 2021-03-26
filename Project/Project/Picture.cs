a93959cba99ddbe61396b8e5f16a61876bd839db
Max <rendon4ik@gmail.com>
2021-03-26 15:57:05 +0200

    Adding 2-nd ctor

--- Project/Project/Picture.cs
+++ Project/Project/Picture.cs
@@ -8,10 +8,16 @@ namespace Project
         public UInt32 Width { get; }
         public UInt32 Depth { get;  }
 
-        public Picture(UInt32 w, UInt32 d)
+        public Picture(UInt32 w, UInt32 d) // 1-st ctor
         {
             Width = w;
             Depth = d;
+        }
+        public Picture(Pixel[,] matrix, int zoomValue) // 2-nd ctor
+        {
+            Depth = matrix.GetLength(0) * zoomValue;
+            Width = matrix.GetLength(1) * zoomValue;
+            Pixels = new Pixel[Depth, Width];
         }
     }
 }
