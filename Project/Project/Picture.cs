9cf2b2cde1cc934298197670295ec5b5a432bda9
Marichka <marichka.v.richtsi@gmail.com>
2021-03-27 20:02:48 +0200

    Fix naming in Picture.cs

--- Project/Project/Picture.cs
+++ Project/Project/Picture.cs
@@ -19,7 +19,7 @@ namespace Project
             Width = Convert.ToUInt32(matrix.GetLength(1) * zoomValue);
             Pixels = new Pixel[Depth, Width];
         }
-        public Pixel[,] Resize(Pixel[,] oldPixels)
+        public void Resize(Pixel[,] oldPixels)
         {
             float widthRatio = (float)(oldPixels.GetLength(1) - 1) / (Width - 1);
             float depthRatio = (float)(oldPixels.GetLength(0) - 1) / (Depth - 1);
@@ -33,15 +33,14 @@ namespace Project
                     int y2 = Convert.ToInt32(Math.Ceiling(depthRatio * i));
                     double xCost = (widthRatio * j) - x1;
                     double yCost = (depthRatio * i) - y1;
-                    Pixel a = oldPixels[y1, x1];
-                    Pixel b = oldPixels[y1, x2];
-                    Pixel c = oldPixels[y2, x1];
-                    Pixel d = oldPixels[y2, x2];
-                    Pixel pixel = a * ((1 - xCost) * (1 - yCost)) + b * ((xCost) * (1 - yCost)) + c * ((1 - xCost) * (yCost)) + d * (xCost * yCost);
+                    Pixel firstPixel = oldPixels[y1, x1];
+                    Pixel secondPixel = oldPixels[y1, x2];
+                    Pixel thirdPixel = oldPixels[y2, x1];
+                    Pixel fourthPixel = oldPixels[y2, x2];
+                    Pixel pixel = firstPixel * ((1 - xCost) * (1 - yCost)) + thirdPixel *((1 - xCost) * (yCost))  + secondPixel * ((xCost) * (1 - yCost)) + fourthPixel * (xCost * yCost);
                     Pixels[i, j] = pixel;
                 }
             }
-            return Pixels;
         }
     }
 }
