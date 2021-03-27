14904055d109f5929795413c1ccd474b0c65a2c6
Max <rendon4ik@gmail.com>
2021-03-27 13:39:55 +0200

    Changes in Picture

--- Project/Project/Picture.cs
+++ Project/Project/Picture.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Project
 {
@@ -15,8 +15,8 @@ namespace Project
         }
         public Picture(Pixel[,] matrix, int zoomValue) // 2-nd ctor
         {
-            Depth = matrix.GetLength(0) * zoomValue;
-            Width = matrix.GetLength(1) * zoomValue;
+            Depth = Convert.ToUInt32(matrix.GetLength(0) * zoomValue);
+            Width = Convert.ToUInt32(matrix.GetLength(1) * zoomValue);
             Pixels = new Pixel[Depth, Width];
         }
         public Pixel[,] Resize(Pixel[,] oldPixels)
@@ -27,17 +27,17 @@ namespace Project
             {
                 for (int j = 0; j < Width; j++)
                 {
-                    int x1 = Math.Floor(widthRatio * j);
-                    int y1 = Math.Floor(depthRatio * i);
-                    int x2 = Math.Ceiling(widthRatio * j);
-                    int y2 = Math.Ceiling(depthRatio * i);
-                    float xCost = (widthRatio * j) - x1;
-                    float yCost = (depthRatio * i) - y1;
-                    UInt32 a = oldPixels[y1, x1];
-                    UInt32 b = oldPixels[y1, x2];
-                    UInt32 c = oldPixels[y2, x1];
-                    UInt32 d = oldPixels[y2, x2];
-                    float pixel = a * (1 - xCost) * (1 - yCost) + b * (xCost) * (1 - yCost) + c * (1 - xCost) * (yCost) + d * xCost * yCost;
+                    int x1 = Convert.ToInt32(Math.Floor(widthRatio * j));
+                    int y1 = Convert.ToInt32(Math.Floor(depthRatio * i));
+                    int x2 = Convert.ToInt32(Math.Ceiling(widthRatio * j));
+                    int y2 = Convert.ToInt32(Math.Ceiling(depthRatio * i));
+                    double xCost = (widthRatio * j) - x1;
+                    double yCost = (depthRatio * i) - y1;
+                    Pixel a = oldPixels[y1, x1];
+                    Pixel b = oldPixels[y1, x2];
+                    Pixel c = oldPixels[y2, x1];
+                    Pixel d = oldPixels[y2, x2];
+                    Pixel pixel = a * ((1 - xCost) * (1 - yCost)) + b * ((xCost) * (1 - yCost)) + c * ((1 - xCost) * (yCost)) + d * (xCost * yCost);
                     Pixels[i, j] = pixel;
                 }
             }
