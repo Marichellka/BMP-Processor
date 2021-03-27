ade894626181f419ef5db285470804b81a693732
Max <rendon4ik@gmail.com>
2021-03-27 13:39:20 +0200

    Adding operator overloading method

--- Project/Project/Pixel.cs
+++ Project/Project/Pixel.cs
@@ -14,5 +14,14 @@ namespace Project
             Green = green;
             Blue = blue;
         }
+        public static Pixel operator *(Pixel item, double number)
+        {
+            byte nb = Convert.ToByte(number);
+            byte red = Convert.ToByte(item.Red);
+            byte green = Convert.ToByte(item.Green);
+            byte blue = Convert.ToByte(item.Blue);
+            return new Pixel((byte)(red * nb), (byte)(green * nb), (byte)(blue * nb));
+        }
+        public static Pixel operator +(Pixel item1, Pixel item2) => new Pixel((byte) (item1.Red + item2.Red), (byte) (item1.Green + item2.Green), (byte) (item1.Blue + item2.Blue));
     }
 }
