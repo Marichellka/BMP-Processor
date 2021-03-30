6fbbb0caf0b50a6d7f9a3b8dfac72a4c0a26c974
Marichka <marichka.v.richtsi@gmail.com>
2021-03-30 23:16:24 +0300

    Change main

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -7,8 +7,11 @@ namespace Project
         static void Main(string[] args)
         {
             BMP_File initialPicture = new BMP_File(args[0]);
+            Console.Write("Enlarging image "+args[2]+" times... ");
             BMP_File resizedPicture = new BMP_File(Convert.ToDouble(args[2]), initialPicture);
+            Console.WriteLine("Done.");
             resizedPicture.Writer(args[1]);
+            Console.WriteLine("Written result to "+args[1]);
         }
     }
 }
